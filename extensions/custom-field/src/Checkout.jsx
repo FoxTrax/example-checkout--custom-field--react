cd9239b39ad60f1b369eca313484a1be79a28cd5
Daniel Glassford <mbushpilot2b@gmail.com>
2024-10-22 09:41:04 -0400

    Working hackery!

--- extensions/custom-field/src/Checkout.jsx
+++ extensions/custom-field/src/Checkout.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   reactExtension,
   TextField,
@@ -6,43 +6,69 @@ import {
   useApplyMetafieldsChange,
   useMetafield,
   Checkbox,
+  useStorage,
 } from "@shopify/ui-extensions-react/checkout";
 
 // [START custom-field.ext-index]
 // Set the entry point for the extension
-export default reactExtension("purchase.checkout.shipping-option-list.render-after", () => <App />);
+export default reactExtension("purchase.checkout.header.render-after", () => <App />);
 // [END custom-field.ext-index]
 
 function App() {
   console.log("App!!!!!!!!!!! OHHEHEHEHEHEEHEHHdJdhalkdsjfhasldkfjahsdflkjh");
-  // [START custom-field.instruction-ui]
-  // Set up the checkbox state
   const [checked, setChecked] = useState(false);
-  // [END custom-field.instruction-ui]
+  console.log("self", self);
 
-  // [START custom-field.define-metafield]
-  // Define the metafield namespace and key
+  const storage = useStorage();
+  console.log("storage", storage);
+
   const metafieldNamespace = "yourAppNamespace";
   const metafieldKey = "deliveryInstructions";
-  // [END custom-field.define-metafield]
 
-  // [START custom-field.use-metafield]
-  // Get a reference to the metafield
   const deliveryInstructions = useMetafield({
     namespace: metafieldNamespace,
     key: metafieldKey,
   });
-  // [END custom-field.use-metafield]
-  // [START custom-field.update-metafield]
-  // Set a function to handle updating a metafield
+
   const applyMetafieldsChange = useApplyMetafieldsChange();
-  // [END custom-field.update-metafield]
 
 
   // Set a function to handle the Checkbox component's onChange event
   const handleChange = () => {
     setChecked(!checked);
   };
+
+  useEffect(() => {
+    const setFXID = async () => {
+      try {
+        const firstCheck = await storage.read("WHOAAAAH");
+        console.log("woah first check!!!!!", firstCheck);
+
+        await storage.write("WHOAAAAH", "1234567890");
+        const fxid = await storage.read("FXID");
+        const whoah = await storage.read("WHOAAAAH");
+        console.log("woah second check!!!!!", whoah);
+
+        console.log("checkout_ext_FXID!!!!!", fxid);
+        if (fxid) {
+          console.log("checkout_ext_FXID_2!!!!!", fxid);
+
+          applyMetafieldsChange({
+            type: "updateMetafield",
+            namespace: "$app",
+            key: "FXID",
+            valueType: "string",
+            value: fxid,
+          });
+        }
+      } catch (error) {
+        console.error("Error reading FXID from storage:", error);
+      }
+    };
+
+    setFXID();
+  }, []);
+
   // [START custom-field.instruction-ui]
   // Render the extension components
   return (
